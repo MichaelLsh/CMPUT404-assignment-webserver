81b9194c59b0305dd54eaaa39676b983f3e78999
Shihao Liu <lsh20011018@gmail.com>
2023-01-27 01:46:22 -0700

    server.py final draft uploaded

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 #  coding: utf-8 
 import socketserver
-import os
-
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+import os 
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Shihao Liu
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,141 +28,89 @@ import os
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
+    
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        """
-        Citation for the following code block
-        Author: Liam Kelly https://stackoverflow.com/users/1987437/liam-kelly
-        URL: https://stackoverflow.com/questions/39090366/how-to-parse-raw-http-request-in-python-3
-        License: MIT License
-        Data&Time: 25/1/2023 @ 5 pm
-        """
-        # ========================================================================================= #
+        # print ("Got a request of: %s\n" % self.data)  
+        # self.request.sendall(bytearray("OK",'utf-8'))
         # Parse the raw request 
-        # fields = self.data.split(b"\r\n")
-        # request_method_type, request_file_path, protocol_version = fields[0].decode("ASCII").split(" ")
-        
-        request_method_type, request_file_path, protocol_version = self.data.decode('utf-8').split("\r\n")[0].split(" ")
-        
-        # headers = fields[1:] # Ignore the GET / HTTP/1.1
-        # output = {}
+        # to get request method, requested file path, protocol version 
+        request_method, requested_file_path, protocol_version = self.data.decode('utf-8').split("\r\n")[0].split(" ")
 
-        # # Split each line by http key-value pairs, decode from bytes to char
-        # for each_header in headers:
-        #     items = each_header.split(b":")
-        #     key = items[0].decode("ASCII")
-        #     values = items[1:]
-        #     for i in range(len(values)):
-        #         values[i] = values[i].decode("ASCII")
-        #     output[key] = values
-        # ========================================================================================= #
-
-        # Determine if request method type is valid 
-        if not self.is_method_processed(request_method_type):
-            status_code = "405 Method Not Allowed"
-            server_response = protocol_version + " " + status_code
-            self.request.sendall(bytearray(server_response, "utf-8"))
-
-        # If the request method type is valid
-        # Determine the requested file type
-        # Attempt to get the requested file locally by the requested file's local path
-        # If the requested file local path is invalid
-        if not self.is_file_type(request_file_path) or self.is_legal_directory(request_file_path):
-            
-            status = "404 Not Found\r\n"
-            server_response =  protocol_version + " " + status
-            self.request.sendall(bytearray(server_response, "utf-8"))
-            return
-
-        # If the requested file's local path is valid
-        local_file_path = self.file_local_path_finder(request_file_path)
-        local_file_type = self.file_content_type_getter(local_file_path)
-        local_file_content = self.file_content_reader(local_file_path)
-
-        # invalid local file type
-        if local_file_type not in ['html', 'css']:
-            if request_file_path.endswith("/"):
-                request_file_path += "index.html"
-            else: 
-                request_file_path += "/"
-                status_code = "301 Moved Permanently\r\n" + "Location: " + request_file_path
-                server_response =  protocol_version + " " + status_code + local_file_type + local_file_content
-                self.request.sendall(bytearray(server_response, "utf-8"))
-                return
-        # The request method and requested file are valid
-        status_code = "200 OK\r\n"
-        server_response =  protocol_version + " " + status_code + local_file_type + local_file_content
-        self.request.sendall(bytearray(server_response, "utf-8"))
-        return
-        # print ("Got a request of: %s\n" % self.data)
-        # self.request.sendall(bytearray("HTTP/1.0 200 OK",'utf-8'))
-
-    def file_local_path_finder(self, request_file_path):
-        local_file_path = "./www" + request_file_path
-        return local_file_path
-
-    def is_method_processed(self, request_method):
-        """
-        Check if our server can handle this request method
-        """
-        # Here only GET request method is valid
+        # Determine if request method is valid
+        # Here only GET request method is acceptable
         if request_method == "GET":
-            return True
-        elif request_method == "PUT":
-            return False
-        elif request_method == "POST":
-            return False
-        elif request_method == "DELETE":
-            return False
-        else:
-            return False
-    
-    def is_file_type(self, request_file_path):
-        """
-        Helper function to identify the requested file type 
-        """
-        # "/foobar.html".split(".")  -> ['/foobar', 'html']
-        request_file_path_components = request_file_path.split(".")
-        if (len(request_file_path_components) > 1): # If the request file path contains a file type
-            return True
-        else: 
-            return False
-    
-    def file_exists(self, request_file_path):
-        local_file_path = "./www" + request_file_path
-        if os.path.isdir(local_file_path) or os.path.isfile(local_file_path):
-            return True
-        else:
-            return False
+            # if requested file exists and can be accessible backwards thru directory
+            if self.file_existence_checker(requested_file_path) and self.backward_dir_access_checker(requested_file_path):
+                # Attempt to get requested file type and content
+                server_response = " 200 OK\r\n"
+                requested_file_type = self.file_type_getter(requested_file_path)
+                if requested_file_type != None: # requested file type is either html or css
+                                                # requested file path targets a file 
+                    requested_file_content = self.file_content_reader(requested_file_path)
+                else: # requested file path targets a file targets a directory
+                    # If requested file path doesn't end with a "/"
+                    if not requested_file_path.endswith("/"):
+                        # lead to a actual file path after adding a "/" at the end of requested file path string
+                        # -> 301
+                        response = protocol_version + " 301 Moved Permanently\r\n"
+                        response += "Location: " + requested_file_path + "/\r\n"
 
-    def is_legal_directory(self, request_file_path):
+                    # If requested file path string ends with "/"
+                    # auto-provide index.html of target directory
+                    requested_file_type = "text/html; charset=utf-8\r\n"
+                    requested_file_content = self.get_content(requested_file_path + "/index.html")
+
+                # generate the headers 
+                server_response += "Connection: Close\r\n"
+                server_response += "Content-Length: " + str(len(requested_file_content)) + "\r\n"
+                server_response += "Content-Type: " + requested_file_type + "\r\n"
+                server_response += "\r\n\n" + requested_file_content
+                self.request.sendall(bytearray(server_response,'utf-8'))
+                return 
+                
+            else: # Cannot locate the requested file -> 404 
+                server_response = protocol_version + " 404 Not Found\r\n"
+                self.request.sendall(bytearray(server_response,'utf-8'))
+                return
+
+        else: # When request method is invalid -> 405 
+            server_response = protocol_version + "405 Method Not Allowed\r\n"
+            self.request.sendall(bytearray("OK",'utf-8'))
+            return 
+    
+
+    def file_existence_checker(self, requested_file_path):
+        """
+        Helper function to check requested file existence
+        """
+        target_file_path = "./www" + requested_file_path
+        return os.path.exists(target_file_path)
+
+    def backward_dir_access_checker(self, requested_file_path):
         """
         Check for backward directory access, ie /../../.. etc
         """
-        dirs = request_file_path.split("/")
+        dirs = requested_file_path.split("/")
         if ".." in dirs:
             return True
         else:
             return False
-
-    def file_content_type_getter(self, local_file_path):
-        file_content_type = "Content: "
-        local_file_path_components = local_file_path.split(".")
-        if len(local_file_path_components) > 1:
-            local_file_content_type = local_file_path_components[1]
+    def file_type_getter(self, requested_file_path):
+        try:
+            requested_file_type = requested_file_path.split(".")[1]
+        except:
+            return None
+        # Here we only consider html and css file types
+        if requested_file_type == "html":
+            return "text/html; charset=utf-8\r\n"
+        elif requested_file_type == "css":
+            return "text/css; charset=utf-8\r\n"
         else: 
-            local_file_content_type = None
-        if local_file_content_type != None:
-            if local_file_content_type == "html":
-                file_content_type += "text/html; charset=utf-8"
-            elif local_file_content_type == "css":
-                file_content_type += "text/css; charset=utf-8"
-        else:
-            local_file_content_type += "text/plain; charset=utf-8"
-        return file_content_type + "\r\n"
+            return "text/plain charset=utf-8\r\n"
 
-    def file_content_reader(self, local_file_path):
-        return"\r\n" + open(local_file_path, "r").read()
+    def file_content_reader(self, requested_file_path):
+        return open( "./www" + requested_file_path, "r").read()
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -171,6 +118,7 @@ if __name__ == "__main__":
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
+
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
